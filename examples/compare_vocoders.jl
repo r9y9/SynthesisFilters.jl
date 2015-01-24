e9740c34279761da4c57e0534b8d9a6245a7e77b
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-01-24 13:11:32 +0900

    Add example: compare_vocoders.jl

---
+++ examples/compare_vocoders.jl
@@ -0,0 +1,88 @@
+# compare vocoders
+
+using MelGeneralizedCepstrums
+using SynthesisFilters
+using WAV
+using DSP
+
+# TODO remove this
+import MelGeneralizedCepstrums: rawdata
+
+# Note about excitation
+# fs: 16000
+# frame period: 5.0 ms
+# F0 analysis: esimated  by WORLD.dio and WORLD.stonemask
+# Excitation genereration: perioic pulse for voiced segments and gaussian random
+# value for un-voiced segments
+base_excitation = vec(readdlm(joinpath(dirname(@__FILE__), "data",
+                                       "test16k_excitation.txt")))
+
+x, fs = wavread(joinpath(dirname(@__FILE__), "data", "test16k.wav"))
+x = vec(x)
+framelen = 512
+hopsize = 80 # 5.0 ms for fs 16000
+noverlap = framelen - hopsize
+
+# Note that mgcep analysis basically assumes power-normalized window
+# ŵ(n) = w(n)|Σₙ w(n)|¹/²
+win = DSP.blackman(framelen) ./ sqrt(sumabs2(DSP.blackman(framelen)))
+
+# create windowed signal matrix that each column represents a windowed time slice
+as = arraysplit(x, framelen, noverlap)
+xw = Array(Float64, framelen, length(as))
+for t=1:length(as)
+    xw[:,t] = as[t]
+end
+# col-wise windowing
+xw .*= win
+
+function test_poledf_synthesis(; order=25, savepath="test16k_poledf.wav")
+    println("testing: poledf_synthesis")
+    # lpc equivalent of mgcep
+    lpc = mgcep(xw, order, 0.0, -1.0; otype=5)
+
+    f = AllPoleDF(order)
+    y = synthesis!(f, base_excitation, rawdata(lpc), hopsize)
+    wavwrite(y, savepath; Fs=fs)
+    println("Dumped to $savepath")
+end
+
+function test_lmadf_synthesis(; order=25, savepath="test16k_lmadf.wav")
+    println("testing: lmadf_synthesis")
+    c = mgcep(xw, order, 0.0, 0.0)
+
+    f = LMADF(order)
+    y = synthesis!(f, base_excitation, rawdata(c), hopsize)
+    wavwrite(y, savepath; Fs=fs)
+    println("Dumped to $savepath")
+end
+
+function test_mlsadf_synthesis(; order=25, savepath="test16k_mlsadf.wav")
+    println("testing: mlsadf_synthesis")
+    α = mcepalpha(fs) # automatic α selection
+    mc = mcep(xw, order, α)
+
+    f = MLSADF(order, α)
+    y = synthesis!(f, base_excitation, rawdata(mc), hopsize)
+    wavwrite(y, savepath; Fs=fs)
+    println("Dumped to $savepath")
+end
+
+function test_mglsadf_synthesis(; order=25, nstage=6,
+                                savepath="test16k_mglsadf.wav")
+    println("testing: mglsadf_synthesis")
+    α = mcepalpha(fs)
+    γ = -1./nstage
+    mgc = mgcep(xw, order, α, γ)
+
+    # waveform synthesis
+    f = MGLSADF(order, α, nstage)
+    y = synthesis!(f, base_excitation, rawdata(mgc), hopsize)
+    wavwrite(y, savepath; Fs=fs)
+    println("Dumped to $savepath")
+end
+
+@time test_poledf_synthesis()
+@time test_lmadf_synthesis()
+@time test_mlsadf_synthesis()
+@time test_mglsadf_synthesis()
